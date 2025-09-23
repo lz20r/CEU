4c1e3eb6976773dd1d61fab9e24a4abc054bb295
lz20r <zhiyaonaiara@gmail.com>
2025-09-23 10:27:26 +0200

    Add new C# program to count from 1 to 10 using while loop

---
+++ Programacion/tema03/Ejercicios/ejercicio05.cs
@@ -0,0 +1,16 @@
+// Contar del 1 al 10 con while
+using System;
+
+public class ContarWhile
+{
+    public static void Main()
+    {
+        int contador = 1;
+
+        while (contador <= 10)
+        {
+            Console.Write(contador + " ");
+            contador++;
+        }
+    }
+}
